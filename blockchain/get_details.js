207662f745e5bdefdb1c4f9dfcf397bae749daf4
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-08-05 13:09:18 +0530

    only commit

---
+++ blockchain/get_details.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+import get_all_Blocks from './node_skellington.js';
+
+
+get_details("vinay");
---
+++ static/popup.js
@@ -0,0 +1,14 @@
+
+// pop up
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    
+    // if()
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
